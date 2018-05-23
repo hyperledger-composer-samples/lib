80c666e6c119d38b61c048866690675542a2a4fa
James Taylor <jamest@uk.ibm.com>
2018-05-23 17:01:27 +0100

    Add script url configuration
    Make the source URL of build scripts configureable via a
    SCRIPT_URL env var
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- scripts/pipeline-PREPARE.sh
+++ scripts/pipeline-PREPARE.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if [ ! -f  ${SCRIPT_DIR} ]; then
+${BUILD_SCRIPTS:?No scripts specified}
+
+if [ ! -f  ${SCRIPT_DIR:=./scripts/} ]; then
   mkdir -p ${SCRIPT_DIR}
 fi
 
 for SCRIPT in $BUILD_SCRIPTS; do
+  SCRIPT_SRC="${SCRIPT_URL:=https://raw.githubusercontent.com/hyperledger-composer-samples/lib/master/scripts}/${SCRIPT}"
   SCRIPT_FILE="${SCRIPT_DIR}${SCRIPT}"
-  SCRIPT_URL="https://raw.githubusercontent.com/hyperledger-composer-samples/lib/master/scripts/${SCRIPT}"
+  
   if [ ! -f  ${SCRIPT_FILE} ]; then
-    echo -e "No script found at ${SCRIPT_FILE}, defaulting to ${SCRIPT_URL}"
-    (curl -sSL ${SCRIPT_URL}) > ${SCRIPT_FILE}
+    echo -e "No script found at ${SCRIPT_FILE}, defaulting to ${SCRIPT_SRC}"
+    (curl -sSL ${SCRIPT_SRC}) > ${SCRIPT_FILE}
   else
     echo "Script found at ${SCRIPT_FILE}"
   fi

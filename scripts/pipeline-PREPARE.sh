f8d1dcd74fded34ce012bfa9b17bdf34c3c5f467
James Taylor <jamest@uk.ibm.com>
2018-06-08 18:56:24 +0100

    Update prepare script
    Copy a default set of scripts
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- scripts/pipeline-PREPARE.sh
+++ scripts/pipeline-PREPARE.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-${BUILD_SCRIPTS:?No scripts specified}
+default_scripts="pipeline-BLOCKCHAIN.sh
+pipeline-BUILD.sh
+pipeline-CLOUDANT.sh
+pipeline-COMMON.sh
+pipeline-DEPLOY.sh"
 
 if [ ! -f  ${SCRIPT_DIR:=./scripts/} ]; then
   mkdir -p ${SCRIPT_DIR}
 fi
 
-for SCRIPT in $BUILD_SCRIPTS; do
-  SCRIPT_SRC="${SCRIPT_URL:=https://raw.githubusercontent.com/hyperledger-composer-samples/lib/master/scripts}/${SCRIPT}"
-  SCRIPT_FILE="${SCRIPT_DIR}${SCRIPT}"
+for script in ${BUILD_SCRIPTS:=$default_scripts}; do
+  SCRIPT_SRC="${SCRIPT_URL:=https://raw.githubusercontent.com/hyperledger-composer-samples/lib/master/scripts}/${script}"
+  SCRIPT_FILE="${SCRIPT_DIR}${script}"
   
   if [ ! -f  ${SCRIPT_FILE} ]; then
     echo -e "No script found at ${SCRIPT_FILE}, defaulting to ${SCRIPT_SRC}"
